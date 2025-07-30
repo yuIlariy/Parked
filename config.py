11efd0395cd0a0278da1405bec9c8af3a1783da0
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2025-07-30 22:03:41 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 BOT_TOKENS = [
-    "123456:ABCDEF",  # Bot 1
-    "987654:ZYXWVU",  # Bot 2
+    "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11",
+    "234567:XYZ-GHI5678mnop-zyx13T8v2g567ef22"
 ]
+
+API_ID = 123456  # ← Replace with your real Telegram API ID
+API_HASH = "abcdef0123456789abcdef0123456789"  # ← Replace with your API Hash
 
 THUMBNAIL_URL = "https://telegra.ph/file/ec17880d61180d3312d6a.jpg"
 REDIRECT_USERNAME = "digitalfilesrenamebot"
-
