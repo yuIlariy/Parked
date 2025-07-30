5be96800509f6a8dcf8840e0021cdc13e8e7bb2d
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2025-07-30 21:49:02 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
+BOT_TOKENS = [
+    "123456:ABCDEF",  # Bot 1
+    "987654:ZYXWVU",  # Bot 2
+]
+
+THUMBNAIL_URL = "https://telegra.ph/file/ec17880d61180d3312d6a.jpg"
+REDIRECT_USERNAME = "digitalfilesrenamebot"
 
